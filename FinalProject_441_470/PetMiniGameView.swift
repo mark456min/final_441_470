162be8bd8415f33390a8cc5cd625d505ea4ebb5b
mark456min <mark456min@gmail.com>
2026-04-26 18:34:04 +0700

    update 3

---
+++ FinalProject_441_470/PetMiniGameView.swift
@@ -0,0 +1,18 @@
+//
+//  PetMiniGameView.swift
+//  FinalProject_441_470
+//
+//  Created by Ativit Tantipisit on 26/4/2569 BE.
+//
+
+import SwiftUI
+
+struct PetMiniGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PetMiniGameView()
+}
